281d40ad9a1b1e0fa49cd77875e73e523f248464
syamsulraa <93592110+syamsulraa@users.noreply.github.com>
2025-05-24 00:33:29 +0700

    script.js

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-zlet viz;
+let viz;
 
-const chartLinks = {
-  BarChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10/Dashboard1',
-  LineChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Line_Chart/Dashboard1',
-  PieChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10_Pie_Chart/Dashboard1',
-  MapChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Map_Chart/Dashboard1',
-  ScatterPlot: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Scatter_Plot/Dashboard1',
-  BubbleChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Bubble_Chart/Dashboard1',
-  TreeMap: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Tree_Map/Dashboard1',
-  GanttChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Gantt_Chart/Dashboard1',
-  LollipopChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Lolipop_Chart/Dashboard1',
-  WordCloud: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Word_Cloud/Dashboard1',
-  BoxPlot: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Box_Pilot/Dashboard1',
-  HistogramChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Histogram_Chart/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link',
-  Dashboard: 'https://public.tableau.com/views/Dashboard_Visualisai_M_Syamsul_Rizal_Amri/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link'
+const vizUrls = {
+  BarChart: "https://public.tableau.com/views/YOUR_BARCHART_URL",
+  LineChart: "https://public.tableau.com/views/YOUR_LINECHART_URL",
+  PieChart: "https://public.tableau.com/views/YOUR_PIECHART_URL",
+  MapChart: "https://public.tableau.com/views/YOUR_MAPCHART_URL",
+  ScatterPlot: "https://public.tableau.com/views/YOUR_SCATTERPLOT_URL",
+  BubbleChart: "https://public.tableau.com/views/YOUR_BUBBLECHART_URL",
+  TreeMap: "https://public.tableau.com/views/YOUR_TREEMAP_URL",
+  GanttChart: "https://public.tableau.com/views/YOUR_GANTTCHART_URL",
+  LollipopChart: "https://public.tableau.com/views/YOUR_LOLLIPOPCHART_URL",
+  WordCloud: "https://public.tableau.com/views/YOUR_WORDCLOUD_URL",
+  BoxPlot: "https://public.tableau.com/views/YOUR_BOXPLOT_URL",
+
+  // ✅ Histogram Chart dengan URL yang kamu berikan
+  Histogram: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Histogram_Chart/Dashboard1",
+
+  Dashboard: "https://public.tableau.com/views/YOUR_DASHBOARD_URL"
 };
 
+function loadViz(chartType) {
+  const containerDiv = document.getElementById("vizContainer");
+  const chartTitle = document.getElementById("chartTitle");
 
+  // Update title
+  chartTitle.innerText = chartType.replace(/([A-Z])/g, " $1").trim();
 
-function loadViz(chartName) {
-  const container = document.getElementById("vizContainer");
-  const title = document.getElementById("chartTitle");
-  const url = chartLinks[chartName];
-
-  title.innerText = chartName.replace(/([A-Z])/g, ' $1').trim();
-
+  // Hapus visualisasi lama jika ada
   if (viz) {
     viz.dispose();
   }
 
+  const url = vizUrls[chartType];
+  if (!url) {
+    containerDiv.innerHTML = `<p>Chart "${chartType}" belum memiliki URL Tableau.</p>`;
+    return;
+  }
+
   const options = {
+    hideTabs: true,
     width: "100%",
-    height: "600px",
-    hideToolbar: false,
-    hideTabs: true
+    height: "800px"
   };
 
-  viz = new tableau.Viz(container, url, options);
+  viz = new tableau.Viz(containerDiv, url, options);
 }
