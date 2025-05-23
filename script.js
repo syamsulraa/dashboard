7fe48c61e62c4d5cdef975f0217c5c0863eb420e
syamsulraa <93592110+syamsulraa@users.noreply.github.com>
2025-05-24 01:46:40 +0700

    script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let viz;
 
 const vizUrls = {
   BarChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10/Dashboard1",
-  LineChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Line_Chart/Dashboard1",
+  LineChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Line_Chart/Dashboard1",
   PieChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10_Pie_Chart/Dashboard1",
-  MapChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Map_Chart/Dashboard1",
-  ScatterPlot: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Scatter_Plot_Chart/Dashboard1",
+  MapChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Map_Chart/Dashboard1",
+  ScatterPlot: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Scatter_Plot/Dashboard1",
   BubbleChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Bubble_Chart/Dashboard1",
   TreeMap: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Tree_Map/Dashboard1",
   GanttChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Gantt_Chart/Dashboard1",
-  LollipopChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Lollipop_Chart/Dashboard1",
+  LollipopChart: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Lolipop_Chart/Dashboard1",
   WordCloud: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Word_Cloud/Dashboard1",
-  BoxPlot: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Box_Plot/Dashboard1",
+  BoxPilot: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Box_Pilot/Dashboard1",
   Histogram: "https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Histogram_Chart/Dashboard1",
   Dashboard: "https://public.tableau.com/views/Dashboard_Visualisai_M_Syamsul_Rizal_Amri/Dashboard1"
 };
