664f045af573ead4e0de8b053ba1343f3f16a9aa
syamsulraa <93592110+syamsulraa@users.noreply.github.com>
2025-05-23 18:35:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let viz;
+
+const chartLinks = {
+  BarChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10/Dashboard1',
+  LineChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Line_Chart/Dashboard1',
+  PieChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011_Project10_Pie_Chart/Dashboard1',
+  MapChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project_10_Map_Chart/Dashboard1',
+  ScatterPlot: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Scatter_Plot/Dashboard1',
+  BubbleChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Bubble_Chart/Dashboard1',
+  TreeMap: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Tree_Map/Dashboard1',
+  GanttChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Gantt_Chart/Dashboard1',
+  LollipopChart: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Lolipop_Chart/Dashboard1',
+  WordCloud: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Word_Cloud/Dashboard1',
+  BoxPlot: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Box_Pilot/Dashboard1',
+  Histogram: 'https://public.tableau.com/views/M_Syamsul_Rizal_Amri2431011Project10_Histogram_Chart/Dashboard1'
+};
+
+function loadViz(chartName) {
+  const container = document.getElementById("vizContainer");
+  const title = document.getElementById("chartTitle");
+  const url = chartLinks[chartName];
+
+  title.innerText = chartName.replace(/([A-Z])/g, ' $1').trim();
+
+  if (viz) {
+    viz.dispose();
+  }
+
+  const options = {
+    width: "100%",
+    height: "600px",
+    hideToolbar: false,
+    hideTabs: true
+  };
+
+  viz = new tableau.Viz(container, url, options);
+}
